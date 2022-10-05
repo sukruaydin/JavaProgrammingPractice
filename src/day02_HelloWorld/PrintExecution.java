74494d63b588538586dd7a80117a49f193468cb4
sukruaydin <sukraydin29@gmail.com>
2022-10-05 10:40:39 +0300

    day02 PrintExecution.java is created

---
+++ src/day02_HelloWorld/PrintExecution.java
@@ -0,0 +1,18 @@
+package day02_HelloWorld;
+
+public class PrintExecution {
+
+    public static void main(String[] args) {
+
+        //appends a new line after execution
+        System.out.println("Ömer Engin");
+        System.out.println("Yusuf Taş");
+
+        //does not append a new line after execution
+        System.out.println();
+        System.out.println("Abdülfındık Samet");
+        System.out.println();
+
+    }
+
+}
