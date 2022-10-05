cb5100a6aad73ee3885f00f5621aa4c37186a740
sukruaydin <sukraydin29@gmail.com>
2022-10-05 11:17:42 +0300

    day03 Err_VS_Out.java PersonalInfo.java Println_VS_Print.java

---
+++ src/day03_EscapeSequences/PersonalInfo.java
@@ -0,0 +1,17 @@
+package day03_EscapeSequences;
+
+public class PersonalInfo {
+
+        public static void main(String[] args) {
+            System.out.println("Name : Aydın");
+            System.out.println("Gender : Male");
+            System.out.println("Date of Birth : 1/6/1993");
+            System.out.println("Adress : Niksar/Tokat");
+            System.out.println("Phone Number : (90)-545-166-37-10");
+            System.out.println("ID number : *******6236");
+
+
+
+    }
+
+}
---
+++ src/day03_EscapeSequences/Println_VS_Print.java
@@ -0,0 +1,24 @@
+package day03_EscapeSequences;
+
+public class Println_VS_Print {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Turkey");
+        System.out.println("How are you all today?");
+        System.out.println();
+        System.out.print("Hello World ");
+        System.out.print("How are you Ramiz Uncle? ");
+        System.out.println("I am good, I am good");
+        System.out.println("--------------------");
+
+        System.out.println("hello guys? how are you all today? we are going to learn escape sequences today. did you all study yesterday?");
+        System.out.println("-------------------------------------------------");
+
+        System.out.print("hello guys? how are you all today? ");
+        System.out.print("we are going to learn escape sequences today. ");
+        System.out.print("did you all study yesterday?");
+
+    }
+
+}
