8e54e88d0f9ba2e6e7af668b7faf12651a7ffe01
sukruaydin <sukraydin29@gmail.com>
2022-10-05 11:09:33 +0300

    day03 AboutMe.java

---
+++ src/day03_EscapeSequences/AboutMe.java
@@ -0,0 +1,11 @@
+package day03_EscapeSequences;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("\tfavorite music : \"black cadillac\"\n\tfavorite book : \"harry potter\"\n\tfavorite quote : \"peace at home peace in the world\"\n\tfavorite tv show : \"black books\"");
+
+    }
+
+}
